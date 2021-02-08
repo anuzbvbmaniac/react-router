cd56a20269b5eb785ff7c9a870ded274382a17c1
anuzbvbmaniac <anuzbvbmaniac123@gmail.com>
2021-02-09 00:29:06 +0545

    Changes: Redirect and 404.

--- src/App.js
+++ src/App.js
@@ -1,53 +1,33 @@
 import React from 'react';
-import { BrowserRouter, NavLink, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Home from "./components/home";
 import Post from "./components/post";
 import Profile from "./components/profile";
 import PostItem from "./components/postitem";
+import Header from "./components/header";
 
 const App = () => {
-  return (
-      <BrowserRouter> {/* <HashRouter> : Adds # before every route || <MemoryRouter> : Saves all the routes to memory but not visible changes shown in uri */}
-        <header>
-          <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-            <h5 className="my-0 mr-md-auto font-weight-normal">Company name</h5>
-            <nav className="my-2 my-md-0 mr-md-3">
-              <NavLink
-                  className="p-2"
-                  to="/"
-                  activeStyle={{color: 'red'}}
-                  activeClassName={'active'}
-                  exact
-              >Home</NavLink>
-              <NavLink
-                  className="p-2"
-                  to="/posts"
-                  activeStyle={{color: 'red'}}
-                  activeClassName={'active'}
-                  exact
-              >Posts</NavLink>
-              <NavLink
-                  className="p-2"
-                  to={{
-                    pathname: '/profile',
-                    hash: '#anuz',
-                    search: '?true=enabled'
-                  }}
-                  activeStyle={{color: 'red'}}
-                  activeClassName={'active'}
-                  exact
-              >Profile</NavLink>
-            </nav>
-          </div>
-        </header>
-        <div className="container">
-          <Route path="/" exact component={Home}/>
-          <Route path="/posts" exact component={Post}/>
-          <Route path="/posts/:id" exact component={PostItem}/>
-          <Route path="/profile" exact component={Profile}/>
-        </div>
-      </BrowserRouter>
-  );
-};
+    return (
+        <BrowserRouter> {/* <HashRouter> : Adds # before every route || <MemoryRouter> : Saves all the routes to memory but not visible changes shown in uri */}
+            <Header />
+            <div className="container">
+                <Switch>
+                    {/*<Redirect from={'/profile'} to={'/'}/>*/}
+                    <Route path="/posts/:id" component={PostItem}/>
+                    <Route path="/posts" component={Post}/>
+                    <Route path="/profile" component={Profile}/>
+                    <Route path="/" exact component={Home}/>
+
+                    <Route render={() => {
+                        return (
+                            <h3>Oops! Page not found!!!</h3>
+                        );
+                    }} />
+                </Switch>
+
+            </div>
+        </BrowserRouter>
+    );
+}
 
 export default App;
